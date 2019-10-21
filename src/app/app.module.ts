38f6bb1b907a2be9adb7d4e79e0758ce9b4b7642
alinul <alin.ungurusan@yahoo.com>
2019-10-21 21:00:18 +0300

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './admin/login/login.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -5,17 +6,40 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule} from 'angularfire2';
 import { AngularFireDatabaseModule} from 'angularfire2/database';
-import { AngularFireAuthModule} from 'angularfire2/auth';
-import { environment } from 'src/environments/environment';
+import { AngularFireAuthModule, AngularFireAuth} from 'angularfire2/auth';
+import { environment } from 'environments/environment';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { RentACarComponent } from './rent-a-car/rent-a-car.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    RentACarComponent,
+    AdminProductsComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent},
+      { path: 'products', component: ProductsComponent},
+      { path: 'rent-a-car', component: RentACarComponent},      
+      { path: 'admin/products', component: AdminProductsComponent},
+      { path: 'admin/login', component: Admin}
+    ]),
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
